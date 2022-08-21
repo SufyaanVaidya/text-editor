4fc9adf28a26264b3868679f67f463fe5424dcdf
Sufyaan Vaidya <vaidyasufyaan@gmail.com>
2022-08-21 18:37:26 -0400

    added git ignore files and some webpack config

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -18,11 +18,44 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      new HtmlWebpackPlugin({
+        template: "./index.html"
+       }),
+       new InjectManifest(),
+       new WebpackPwaManifest({
+        name: "Text Directory",
+        short_name: 'Directory',
+        description: 'My awesome Progressive Web App!',
+        background_color: '#ff0000',
+        start_url: './',
+        icons: [
+          {
+            src: path.resolve('./src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('images', 'icons'),
+          }
+        ]
+       })
+
     ],
 
     module: {
       rules: [
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+            },
+          },
+        },
         
       ],
     },
