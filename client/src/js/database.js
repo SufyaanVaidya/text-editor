b8729d75c59c657795f21ed8814693abe01c7b50
Sufyaan Vaidya <vaidyasufyaan@gmail.com>
2022-08-22 20:13:01 -0400

    added logic for getdb

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,17 +12,25 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
+//  Added logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
   const jateDb = await openDB('jate', 1);
   const tx = jateDb.transaction('jate', 'readwrite');
   const store = tx.objectStore('jate');
-  const request = store.put({ data: content});
+  const request = store.put({ data: content });
   const result = await request;
   console.log('data saved', result)
 };
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+// Added logic for a method that gets all the content from the database
+export const getDb = async () => {
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
+  const request = store.get(1);
+  const result = await request;
+  console.log('data saved', result)
+
+}
 
 initdb();
